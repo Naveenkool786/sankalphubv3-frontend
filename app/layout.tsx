c9f6fa5625d7b511cb9b6576d247c27e4bfb545d
root <root@srv1372067.hstgr.cloud>
2026-03-27 18:51:56 +0000

    Add Google Analytics G-YLHER20GW0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Sora, DM_Sans } from "next/font/google"
 import { Toaster } from "sonner"
 import { ThemeProvider } from "@/components/ThemeProvider"
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css"
 
 const sora = Sora({ subsets: ["latin"], variable: "--font-sora" })
@@ -25,6 +26,7 @@ export default function RootLayout({
           <Toaster richColors position="top-right" />
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-YLHER20GW0" />
     </html>
   )
 }
